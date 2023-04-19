b890424ad6e7e1c075c503d81841380c84bd1544
igna <ignas.gomezb12@gmail.com>
2023-04-19 10:09:05 -0300

    ej2 resuelto

--- ej2/proc1.c
+++ ej2/proc1.c
@@ -2,16 +2,22 @@
 #include <stdio.h>
 
 void absolute(int x, int y) {
-    //
-    // Completar aquí
-    //
+    if (x >= 0){
+        y = x;
+    }
+    else {
+        y = (-1) * x;
+    }
 }
 
 int main(void) {
     int a=0, res=0;
-    //
-    // Completar aquí
-    //
+    a = -10;
+    absolute(a, res);
+    printf("El valor de res es %i\n", res);
     return EXIT_SUCCESS;
 }
+
+//El resultado imprimido en pantalla no es el deseado 
+//no coincide con en el programa del lenguaje utilizado en el teorico (ya que ese si retorna el valor correcto)
 
--- ej2/proc2.c
+++ ej2/proc2.c
@@ -2,16 +2,26 @@
 #include <stdio.h>
 
 void absolute(int x, int *y) {
-    //
-    // Completar aquí
-    //
+    if (x >= 0){
+        *y = x;
+    }
+    else {
+        *y = (-1) * x;
+    }
 }
 
 int main(void) {
+    int *p;
     int a=0, res=0;  // No modificar esta declaración
-    //
-    // Completar aquí
-    //
+    a = -10;
+
+    p = &res;
+
+    absolute(a, p);
+    printf("El valor de res es %d\n", res);
     return EXIT_SUCCESS;
 }
 
+//Este programa si retorna los valores esperados para la variable res
+//El parámetro int *y de absolute() es de tipo out si se mirara con el lenguaje del teorico, no me importa su contenido, solo le asigno un nuevo valor
+//C solo tiene parametros de tipo in para sus funciones, estas pueden leer las variables locales de mi programa pero no pueden modificarlas por fuera de la misma funcion;
