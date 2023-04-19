ca82602b9234da67803d36a26f8e935fcba73712
igna <ignas.gomezb12@gmail.com>
2023-04-19 10:48:56 -0300

    comentarios al final del codigo del ej

--- ej2/proc2.c
+++ ej2/proc2.c
@@ -24,4 +24,5 @@ int main(void) {
 
 //Este programa si retorna los valores esperados para la variable res
 //El parámetro int *y de absolute() es de tipo out si se mirara con el lenguaje del teorico, no me importa su contenido, solo le asigno un nuevo valor
-//C solo tiene parametros de tipo in para sus funciones, estas pueden leer las variables locales de mi programa pero no pueden modificarlas por fuera de la misma funcion;
+//C solo tiene parametros de tipo in para sus funciones, estas pueden leer las variables locales de mi programa pero no pueden modificarlas por fuera de la misma funcion
+//Se pueden modificar variables locales con una funcion, pero es necesario pasar parametros por referencia con punteros
