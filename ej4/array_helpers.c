3dc3c5c779dfbbdd7c060ef19f6e7d883294cc87
igna <ignas.gomezb12@gmail.com>
2023-04-20 11:32:37 -0300

    ejercicio 4, terminado, sin errores

--- ej4/array_helpers.c
+++ ej4/array_helpers.c
@@ -59,6 +59,7 @@ void array_from_file(WeatherTable array, const char *filepath) {
         }
         Weather weather = weather_from_file(file);
         /* Completar acá: Guardar la medición de clima en el arreglo multidimensional */
+        array[k_year - FST_YEAR][k_month - 1][k_day - 1]= weather;
     }
     fclose(file);
 }
